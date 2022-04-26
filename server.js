20ae2dd1105a7a12ecbf72a4ea9b7fe4a5638f91
Becca Yu <ugsung8970@gmail.com>
2022-04-25 21:18:51 -0700

    commiting changes after correcting serverjs file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./config/connection')
 //const routes = require('./routes')
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 
 app.use(express.urlencoded({ extended: true}));
@@ -11,7 +11,7 @@ app.use(express.json());
 
 //app.use(express.static('public'));
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
